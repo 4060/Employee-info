642769d6443a1bd5aeabd3f3dbdef2c1913b2604
vamshi47 <vamshiyalavarthi11@gmail.com>
2018-02-24 18:55:13 +0530

    Add files via upload

--- EmpService.java
+++ EmpService.java
@@ -1,18 +1,38 @@
-package com.emp;
- 
-import java.util.List;
- 
-import com.emp.Emp;
- 
-public interface EmpService {
- 
-public List listAllUser();
- 
- public void addUser(Emp user);
- 
- public void updateUser(Emp user);
- 
- public void delete(Emp user);
- 
- public Emp findUserById(Emp user);
+package com.vamshi.restfulapi;
+
+import org.springframework.stereotype.Service;
+
+import com.vamshi.restfulapi.Emp;
+import com.vamshi.restfulapi.EmpRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class EmpService {
+
+    @Autowired
+    private EmpRepository empRepository;
+
+    public Object findAll(){
+        return empRepository.findAll();
+    }
+
+    public Emp findById(Integer id){
+        return empRepository.findOne(id);
+    }
+
+    public Emp save(Emp emp){
+        return empRepository.save(emp);
+    }
+
+    public void delete(Emp emp){
+    	empRepository.delete(emp);
+    	return;
+    }
+
+	public Emp findByEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
 }
